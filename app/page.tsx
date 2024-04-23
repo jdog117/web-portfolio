679b02d6b82073f496d46ef2ee605506645335c0
jdog117 <jspcasey@gmail.com>
2024-04-23 11:13:27 -0700

    gsap header

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
+
 export default function Home() {
     return (
         <>
@@ -10,15 +18,29 @@ export default function Home() {
 }
 
 function Hero() {
+    const container = useRef<HTMLDivElement>(null);
+    useGSAP(
+        () => {
+            gsap.fromTo(
+                ".p-title",
+                { letterSpacing: "1em" },
+                { letterSpacing: "0.39em", duration: 1.5, ease: "power2.inOut" }
+            );
+        },
+        { scope: container }
+    );
+
     return (
         <div className="w-full h-screen items-center justify-center flex">
-            <div className="flex flex-col items-end">
-                <h1 className="text-4xl text-right mr-16 mb-2">JOEY CASEY</h1>
-                <h1 className="text-9xl font-extrabold tracking-[0.5em] ">
+            <div ref={container} className="container flex flex-col relative">
+                <h1 className="text-4xl text-right mr-32 mb-2 justify-end flex">
+                    JOEY CASEY
+                </h1>
+                <h1 className="p-title text-9xl font-extrabold tracking-[1em] justify-center flex">
                     PORTFOLIO
                 </h1>
-                <div className="absolute flex flex-col mt-20 font-bold text-stroke text-transparent">
-                    <h1 className="text-8xl ">DEV</h1>
+                <div className="absolute flex flex-col mt-28 font-bold text-stroke text-transparent mr-8 right-0">
+                    <h1 className="text-8xl">DEV</h1>
                     <h1 className="text-8xl">DESIGN</h1>
                 </div>
             </div>
