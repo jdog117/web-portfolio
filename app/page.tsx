f68d6a7a13c9f5f88a1939f054953c418748be9e
jdog117 <jspcasey@gmail.com>
2024-04-23 11:47:58 -0700

    fixed FOUC
    used autoAplha to fix the text flashing on screen on page load

--- app/page.tsx
+++ app/page.tsx
@@ -26,6 +26,12 @@ function Hero() {
                 { letterSpacing: "1em" },
                 { letterSpacing: "0.39em", duration: 1.5, ease: "power2.inOut" }
             );
+            gsap.from(".p-title", {
+                duration: 2,
+                opacity: 0,
+                autoAlpha: 0,
+                ease: "power2.inOut",
+            });
         },
         { scope: container }
     );
@@ -36,7 +42,7 @@ function Hero() {
                 <h1 className="text-4xl text-right mr-32 mb-2 justify-end flex">
                     JOEY CASEY
                 </h1>
-                <h1 className="p-title text-9xl font-extrabold tracking-[1em] justify-center flex">
+                <h1 className="p-title text-9xl font-extrabold tracking-[1em] justify-center flex invisible">
                     PORTFOLIO
                 </h1>
                 <div className="absolute flex flex-col mt-28 font-bold text-stroke text-transparent mr-8 right-0">
