1e787770c0a804232094b8d02305418b9833f07b
jdog117 <jspcasey@gmail.com>
2024-04-22 23:40:00 -0700

    STROKE LETS GOO
    FInally figured out how to add stroke to text. Using webkit

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,15 @@ export default function Home() {
 function Hero() {
     return (
         <div className="w-full h-screen items-center justify-center flex">
-            <div className="flex flex-col">
+            <div className="flex flex-col items-end">
                 <h1 className="text-4xl text-right mr-16 mb-2">JOEY CASEY</h1>
                 <h1 className="text-9xl font-extrabold tracking-[0.5em] ">
                     PORTFOLIO
                 </h1>
+                <div className="absolute flex flex-col mt-20 font-bold text-stroke text-transparent">
+                    <h1 className="text-8xl ">DEV</h1>
+                    <h1 className="text-8xl">DESIGN</h1>
+                </div>
             </div>
         </div>
     );
@@ -24,16 +28,18 @@ function Hero() {
 
 function Regenmed() {
     return (
-        <>
-            <div className="w-full h-screen items-center justify-center flex bg-secondary"></div>
-        </>
+        <div className="w-full h-screen items-center justify-center flex bg-secondary"></div>
     );
 }
 
 function Fund() {
-    return <></>;
+    return (
+        <div className="w-full h-screen items-center justify-center flex bg-background"></div>
+    );
 }
 
 function Emailfinder() {
-    return <></>;
+    return (
+        <div className="w-full h-screen items-center justify-center flex bg-secondary"></div>
+    );
 }
