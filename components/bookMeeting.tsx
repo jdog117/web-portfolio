795ee4301182c2e877a82a2316f901eaf3bb9179
jdog117 <jspcasey@gmail.com>
2024-05-07 16:10:46 -0700

    calendly drawer working

--- components/bookMeeting.tsx
+++ components/bookMeeting.tsx
@@ -1,10 +1,41 @@
 "use client";
+
 import { Button } from "@/components/ui/button";
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer";
+import { InlineWidget } from "react-calendly";
 
 export default function Book() {
     return (
-        <Button className="fixed bottom-0 right-0 p-6 md:px-9 md:py-6 z-10 m-8 md:m-16 rounded-full">
-            Let{`'`}s talk!
-        </Button>
+        <>
+            <Drawer>
+                <DrawerTrigger asChild>
+                    <Button className="fixed bottom-0 right-0 p-6 md:px-10 md:py-7 z-10 m-8 md:m-16 rounded-full">
+                        Let{`'`}s talk!
+                    </Button>
+                </DrawerTrigger>
+                <DrawerContent>
+                    <InlineWidget
+                        styles={{ height: "70vh" }}
+                        url="https://calendly.com/jspcasey"
+                    />
+                    <DrawerFooter className="items-center">
+                        <DrawerClose asChild>
+                            <Button variant="outline" className="w-min ">
+                                Close
+                            </Button>
+                        </DrawerClose>
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
+        </>
     );
 }
